8dcc123f07532ec22f2d0c5876823bab96deadcc
TanyaPetkova <taniapetkova@abv.bg>
2014-09-03 12:20:58 +0300

    PDF Report Generation

--- Weapons Factory - The Project/WeaponsFactory.ConsoleClient/WeaponsFactoryConsoleClient.cs
+++ Weapons Factory - The Project/WeaponsFactory.ConsoleClient/WeaponsFactoryConsoleClient.cs
@@ -9,9 +9,15 @@
     using WeaponsFactory.Models;
     using WeaponsFactory.DataAccess;
     using WeaponsFactory.MongoDb;
+    using WeaponsFactory.Data.PDFReportGeneration;
 
     public class WeaponsFactoryConsoleClient
     {
+        public const string PdfReportfPath = "../../";
+        public const string PdfReportName = "/Report.pdf";
+
+        private static readonly IWeaponsFactoryData weaponsFactoryData = new WeaponsFactoryData();
+
         public static void Main()
         {
             //SQL
@@ -52,6 +58,14 @@
             //{
             //    Console.WriteLine("date: {0}, Quantity: {1}, SaleId {2}, UnitePrice: {3}, Vendor: {4}, VendorID: {5}, Weapon: {6}, WeaponID {7}", sale.Date, sale.Quantity, sale.SaleId, sale.UnitPrice, sale.Vendor, sale.VendorId, sale.Weapon, sale.WeaponId);
             //}
+
+            GeneratePDFReport();
+        }
+
+        private static void GeneratePDFReport()
+        {
+            var prdfReportGenerator = new PDFReportGenerator(weaponsFactoryData);
+            prdfReportGenerator.ExportSalesEntriesToPdf(PdfReportfPath, PdfReportName, new DateTime(2000, 1, 1), new DateTime(2014, 1, 1));
         }
     }
 }
---
+++ Weapons Factory - The Project/WeaponsFactory.Models/PdfSaleReportEntry.cs
@@ -0,0 +1,22 @@
+﻿namespace WeaponsFactory.Reports.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public class PdfSaleReportEntry
+    {
+        public string Name { get; set; }
+
+        public int Quantity { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Vendor { get; set; }
+
+        [Column("Unit Price")]
+        public decimal UnitPrice { get; set; }
+
+        public decimal Sum { get; set; }
+    }
+}
